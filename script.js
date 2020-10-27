73908d8ab10b6cd3a2deb27d719e8b6635551d59
lilgujj <gustavkarlandersson@hotmail.com>
2020-10-27 17:02:50 +0100

    betygskrav

---
+++ script.js
@@ -0,0 +1,16 @@
+
+// Krav för godkänt:d
+// 1. Prompt används för att be användaren om kommandon
+// 2. Alert används för ge information till användaren om världen
+// 3. Git & GitHub har använts
+// 4. Projektmappen innehåller en README.md fil - (läs ovan för mer info)
+// 5. Uppgiften lämnas in i tid!
+
+// Krav för väl godkänt:
+// 1. Alla punkter för godkänt är uppfyllda
+// 2. Textfält & knappar används för att ange kommandon (istället för Prompt)
+// 3. Information till användaren presenteras direkt på sidan (istället för i en Alert)
+// 4. Alla funktioner och objekt är dokumenterade med korrekt JS-doc
+// 5. Det ska finnas flera olika vägval att göra för användaren i din värld
+
+
