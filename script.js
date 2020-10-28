57dc0fc994f877ea4ca320c14269b7489b37dac7
lilgujj <gustavkarlandersson@hotmail.com>
2020-10-28 15:14:26 +0100

    working script

--- script.js
+++ script.js
@@ -14,5 +14,137 @@
 // 5. Det ska finnas flera olika vägval att göra för användaren i din värld
 
 
+const textElement = document.getElementById("text")
+const optionsButtonElement = document.getElementById("btn-options")
 
 
+function startGame() {
+    showTextNode(0)
+
+}
+
+
+function showTextNode(textNodeIndex) {
+    let textNode = textNodes.find(textNode => textNode.id === textNodeIndex)
+    textElement.innerText = textNode.text
+    while (optionsButtonElement.firstChild) {
+        optionsButtonElement.removeChild(optionsButtonElement.firstChild)
+    }
+
+
+    textNode.options.forEach(option => {
+        if (showOption(option)) {
+            const button = document.createElement("button")
+            button.innerText = option.text
+            button.classList.add("btn")
+            button.addEventListener("click", () => selectOption(option) )
+            optionsButtonElement.appendChild(button)
+        }
+    })
+}
+
+
+function showOption (option) { 
+    return true
+
+} 
+
+function selectOption(option) {
+    const nextTextNodeId = option.nextText
+    showTextNode(nextTextNodeId)
+
+}
+
+
+
+const textNodes = [
+    {
+        id: 0,
+        text: "Välkommen till Äventyret i Djungeln",
+        options: [
+            {
+                text: "Börja spela",
+                nextText: 1
+            }
+        ]
+
+    },
+    {
+        id: 1,
+        text: "Du är vilse i djungeln och ser ett svärd",
+        options: [
+            {
+                text: "ta svärdet",
+                nextText: 2,
+            },
+            {
+                text: "lämna svärdet",
+                nextText: 0,
+            },
+        ]
+    },
+    {
+        id: 2,
+        text: " Du håller nu i ett svärd och stöter på din kompis ",
+        options: [
+            {
+                text: "ge din kompis ditt svärd",
+                nextText: 3,
+            },
+            {
+                text: "döda din kompis",
+                nextText: 4,
+            },
+            {
+                text: "hjälp din kompis komma ur djungeln",
+                nextText: 5,
+            }
+        ]
+    },
+    {
+        id: 3,
+        text: "Du gav ditt svärd till din kompis, Din kompis vet inte vem han är för han har varit vilse för länge så han dödar dig",
+        options: [
+            {
+                text: "du dog, prova igen",
+                nextText: 0
+            }
+        ]
+    },
+    {
+        id: 4,
+        text: "du dödade din kompis. Nu kommer det två poliser och ser din kompis som är död",
+        options: [
+            {
+                text: "du erkänner mordet och börja om",
+                nextText: 0
+            }
+        ]
+    },
+    {
+        id: 5,
+        text: "Du och din kompis använder svärdet för att hitta en väg ut. Ni hittade fram till stranden och eran båt är där",
+        options: [
+            {
+                text: "hoppa på båten och åk iväg",
+                nextText: 6
+            },
+            {
+                text: "gå in i djungeln och börja om",
+                nextText: 0
+            }
+        ]
+    },
+    {
+        id: 6,
+        text: "Grattis du klarade spelet",
+        options: [
+            {
+                text: "spela igen",
+                nextText: 0,
+            }
+        ]
+    }
+]
+
+startGame();
