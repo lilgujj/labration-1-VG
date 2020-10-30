f2ce38b14835f973dc32fe270be642d7d0d0b517
lilgujj <gustavkarlandersson@hotmail.com>
2020-10-30 16:32:32 +0100

    more

--- script.js
+++ script.js
@@ -1,24 +1,9 @@
 
-// Krav för godkänt:d
-// 1. Prompt används för att be användaren om kommandon
-// 2. Alert används för ge information till användaren om världen
-// 3. Git & GitHub har använts
-// 4. Projektmappen innehåller en README.md fil - (läs ovan för mer info)
-// 5. Uppgiften lämnas in i tid!
-
-// Krav för väl godkänt:
-// 1. Alla punkter för godkänt är uppfyllda
-// 2. Textfält & knappar används för att ange kommandon (istället för Prompt)
-// 3. Information till användaren presenteras direkt på sidan (istället för i en Alert)
-// 4. Alla funktioner och objekt är dokumenterade med korrekt JS-doc
-// 5. Det ska finnas flera olika vägval att göra för användaren i din värld
-
-
 const textElement = document.getElementById("text")
 const optionsButtonElement = document.getElementById("btn-options")
 
 /**
- * startar spelet
+ * startar spelet på scen 0
  */
 function startGame() {
     showTextNode(0)
@@ -30,6 +15,7 @@ function startGame() {
  * @param {Number} textNodeIndex 
  * denna funktion tar bort alla options dvs buttons som inte har med 
  * vilken textnode/scen du är på. 
+ * lägger till texten från vilken textNode du är på till rätt scen.
  */
 function showTextNode(textNodeIndex) {
     let textNode = textNodes.find(textNode => textNode.id === textNodeIndex)
